8788f53d4e9564e8950cc08d05991ed080e6b1b3
Francesco Murdaca <fmurdaca@redhat.com>
2020-10-06 12:13:37 +0200

    Select adviser-version (#70)
    * Select adviser-version
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>
    
    * Adjust tests
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>
    
    * Makre pre-commit happy
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- thoth/report_processing/components/security.py
+++ thoth/report_processing/components/security.py
@@ -267,9 +267,9 @@ class SecurityIndicatorsBandit(_SecurityIndicators):
             "analyzer_si_bandit": report_metadata["analyzer"],
             "analyzer_version_si_bandit": report_metadata["analyzer_version"],
             "document_id_si_bandit": report_metadata["document_id"],
-            "package_name": report_metadata["arguments"]["app.py"]["package_name"],
-            "package_version": report_metadata["arguments"]["app.py"]["package_version"],
-            "package_index": report_metadata["arguments"]["app.py"]["package_index"],
+            "package_name": report_metadata["arguments"]["si-bandit"]["package_name"],
+            "package_version": report_metadata["arguments"]["si-bandit"]["package_version"],
+            "package_index": report_metadata["arguments"]["si-bandit"]["package_index"],
         }
 
         return extracted_metadata
@@ -626,9 +626,9 @@ class SecurityIndicatorsCloc:
             "analyzer_si_cloc": report_metadata["analyzer"],
             "analyzer_version_si_cloc": report_metadata["analyzer_version"],
             "document_id_si_cloc": report_metadata["document_id"],
-            "package_name": report_metadata["arguments"]["app.py"]["package_name"],
-            "package_version": report_metadata["arguments"]["app.py"]["package_version"],
-            "package_index": report_metadata["arguments"]["app.py"]["package_index"],
+            "package_name": report_metadata["arguments"]["si-cloc"]["package_name"],
+            "package_version": report_metadata["arguments"]["si-cloc"]["package_version"],
+            "package_index": report_metadata["arguments"]["si-cloc"]["package_index"],
         }
 
         return extracted_metadata
